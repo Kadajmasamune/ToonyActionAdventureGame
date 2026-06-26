a07599b2c64ea18c3e7a982dced85fbf84778938
Rayan Ul Haq (12th Remnant) <150000465+Kadajmasamune@users.noreply.github.com>
2026-06-26 13:19:26 +0500

    state machine decoupled

---
+++ ToonyActionAdventureGame/Assets/Scripts/Attacks/AttackExecution.cs
@@ -0,0 +1,348 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static FrameData;
+
+public class AttackExecution : MonoBehaviour
+{
+    private Attack currentAttack;
+
+    private IWeapon weaponHandler;
+    private AnimatorController animator;
+
+    private HashSet<Collider> hitTargets = new();
+
+    private int attackStartTick;
+
+
+    private class ImpactData
+    {
+        public Vector3 start;
+        public Vector3 end;
+        public float timer;
+        public float duration;
+        public AnimationCurve curve;
+    }
+
+
+    private Dictionary<Collider, ImpactData> impacts = new();
+    private List<Collider> finishedImpacts = new();
+
+
+    public bool IsExecuting { get; private set; }
+    public bool IsTransitioning { get; set; }
+
+
+    public int CurrentTick
+    {
+        get
+        {
+            return Ticker.instance.CurrentTick - attackStartTick;
+        }
+    }
+
+
+    public Attack CurrentAttack => currentAttack;
+
+
+    public void Initialize(IWeapon weapon, AnimatorController anim)
+    {
+        weaponHandler = weapon;
+        animator = anim;
+    }
+
+
+    private void OnEnable()
+    {
+        Ticker.OnTick += Tick;
+    }
+
+
+    private void OnDisable()
+    {
+        Ticker.OnTick -= Tick;
+    }
+
+
+
+    public void StartAttack(Attack attack, bool transition = false)
+    {
+        currentAttack = attack;
+
+        IsExecuting = true;
+        IsTransitioning = transition;
+
+        attackStartTick = Ticker.instance.CurrentTick;
+
+        hitTargets.Clear();
+
+        weaponHandler.DisableHitbox();
+        weaponHandler.ClearTargets();
+
+
+        int hash = Animator.StringToHash(
+            attack.clip.name
+        );
+
+
+        animator.PlayAttack(
+            hash,
+            attack.AttackIndexLayer,
+            transition
+        );
+    }
+
+
+
+    private void Tick()
+    {
+        if (!IsExecuting)
+            return;
+
+
+        UpdateImpacts();
+
+
+        int tick = CurrentTick;
+
+
+        UpdateWeapon(tick);
+
+        ProcessHits();
+
+
+        if (tick >= TotalFrames())
+        {
+            End();
+        }
+    }
+
+
+
+    private int TotalFrames()
+    {
+        return
+            currentAttack.StartUpFrames +
+            currentAttack.ActiveFrames +
+            currentAttack.RecoveryFrames;
+    }
+
+
+
+    private void UpdateWeapon(int tick)
+    {
+        int start =
+            currentAttack.StartUpFrames;
+
+
+        int end =
+            start + currentAttack.ActiveFrames;
+
+
+
+        if (tick >= start && tick <= end)
+            weaponHandler.EnableHitbox();
+        else
+            weaponHandler.DisableHitbox();
+    }
+
+
+
+
+    private void ProcessHits()
+    {
+        foreach (Collider target in weaponHandler.Targets)
+        {
+            if (hitTargets.Contains(target))
+                continue;
+
+
+            hitTargets.Add(target);
+
+
+            Health health =
+                target.GetComponent<Health>();
+
+
+            if (health)
+                health.TakeDamage(
+                    currentAttack.Damage
+                );
+
+
+            if (currentAttack.upwardLaunchImpulse > 0)
+                Launch(target);
+            else
+                KnockBack(target);
+
+
+
+            StartCoroutine(
+                HitStop()
+            );
+        }
+    }
+
+
+
+
+
+    private void Launch(Collider target)
+    {
+        Vector3 start =
+            target.transform.position;
+
+
+        Vector3 end =
+            start +
+            Vector3.up *
+            currentAttack.upwardLaunchImpulse;
+
+
+        impacts[target] = new ImpactData
+        {
+            start = start,
+            end = end,
+            duration = currentAttack.attackSpeed,
+            curve = currentAttack.lungeCurve
+        };
+    }
+
+
+
+
+    private void KnockBack(Collider target)
+    {
+        Vector3 start =
+            target.transform.position;
+
+
+        Vector3 dir =
+            (target.transform.position -
+             transform.position).normalized;
+
+
+
+        impacts[target] = new ImpactData
+        {
+            start = start,
+            end =
+                start +
+                dir *
+                currentAttack.knockBackInflicted,
+
+            duration =
+                currentAttack.attackSpeed,
+
+            curve =
+                currentAttack.lungeCurve
+        };
+    }
+
+
+    public WindowType DeduceCurrentWindow(int tick, Attack currentAttack)
+    { 
+        if (currentAttack == null)
+            return WindowType.None; 
+        for (int i = 0; i < currentAttack.FrameWindows.Length; i++) 
+        {
+            if (currentAttack.FrameWindows[i].windowType == WindowType.Interrupt && 
+                (tick >= currentAttack.FrameWindows[i].startFrame && tick <= currentAttack.FrameWindows[i].endFrame))
+                return WindowType.Interrupt; 
+
+            else if (currentAttack.FrameWindows[i].windowType == WindowType.Invulnerability && 
+                (tick >= currentAttack.FrameWindows[i].startFrame && tick <= currentAttack.FrameWindows[i].endFrame)) 
+                return WindowType.Invulnerability; 
+
+        } 
+        return WindowType.None; 
+    
+    }
+
+
+    private void UpdateImpacts()
+    {
+        finishedImpacts.Clear();
+
+
+        foreach (var pair in impacts)
+        {
+            pair.Value.timer += Time.deltaTime;
+
+
+            float t =
+                Mathf.Clamp01(
+                    pair.Value.timer /
+                    pair.Value.duration
+                );
+
+
+            float eased =
+                pair.Value.curve.Evaluate(t);
+
+
+
+            if (pair.Key)
+            {
+                pair.Key.transform.position =
+                    Vector3.Lerp(
+                        pair.Value.start,
+                        pair.Value.end,
+                        eased
+                    );
+            }
+
+
+            if (pair.Value.timer >= pair.Value.duration)
+                finishedImpacts.Add(pair.Key);
+        }
+
+
+
+        foreach (var c in finishedImpacts)
+            impacts.Remove(c);
+    }
+
+
+
+
+
+    private IEnumerator HitStop()
+    {
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(
+            currentAttack.hitStopDuration
+        );
+
+        Time.timeScale = 1f;
+    }
+
+
+
+
+
+    public void End()
+    {
+        int hash =
+            Animator.StringToHash(
+                currentAttack.clip.name
+            );
+
+
+        animator.StopAttack(hash);
+
+
+        weaponHandler.DisableHitbox();
+        weaponHandler.ClearTargets();
+
+
+        hitTargets.Clear();
+
+
+        currentAttack = null;
+
+        IsExecuting = false;
+        IsTransitioning = false;
+    }
+}
--- ToonyActionAdventureGame/Assets/Scripts/Attacks/CombatStateMachine.cs
+++ ToonyActionAdventureGame/Assets/Scripts/Attacks/CombatStateMachine.cs
@@ -1,136 +1,133 @@
-﻿using Common;
-using Mono.Cecil;
-using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.Profiling;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using static FrameData;
 using static InputBufferer;
 
-public class CombatStateMachine : MonoBehaviour // ----> “Given state + input + context → what happens next?”                                                
+public class CombatStateMachine : MonoBehaviour
 {
-
-
-    // -- > Implement stricter flow of attacks 
-    // -- > Remove Spamming input and attack
-
-
-    // -- > Begin Implementing Direction Modifiers 
-    // -- > Timing Between inputs Modifiers 
-
     [Header("Attack")]
-    //public Attack[] PossibleAttacks;
     public Attack CurrentAttack;
-    public bool isAttacking = false;
-    public bool isTransitioning = false;
-    public bool hasChainedThisWindow = false;
-    public bool shouldLaunchTarget => (CurrentAttack.upwardLaunchImpulse > 0 && CurrentAttack.knockBackInflicted <= 0);
-    public int CurrentAttackTick {  get; private set; }
-    private IReadOnlyList<Collider> targetsHit => weaponHanlder.Targets;
-    private HashSet<Collider> hitTargets = new HashSet<Collider>();
-    private class ImpactData
-    {
-        public Vector3 start;
-        public Vector3 end;
-        public float timer;
-        public float duration;
-        public AnimationCurve curve;
-    }
-    private Dictionary<Collider, ImpactData> impacts = new();
-    private List<Collider> finishedImpacts = new();
-    public bool isTargetHit;
-    private bool isTryingToCancel = false;
 
+    public bool isAttacking;
+    public bool isTransitioning;
+    public bool hasChainedThisWindow;
 
-    [Header("Weapon")]
-    //public Weapon currentWeapon;
-    //public WeaponHandling currentWeapon;
-    private IWeapon weaponHanlder;
-    private ICombatHandler handler;
-    public Attack.Context[] currentContext;
-    
+    public int CurrentAttackTick =>
+        execution != null ? execution.CurrentTick : 0;
 
-
-    [Header("Ticker Config")]
-    private int attackStartTick;
 
     [Header("References")]
     [SerializeField] private InputBufferer bufferer;
+
     private AnimatorController animator;
+
+    private IWeapon weaponHandler;
+    private ICombatHandler handler;
+
+    private AttackExecution execution;
+
+    private bool isTryingToCancel;
+
+
 
     public void Initialize(ICombatHandler handler)
     {
         this.handler = handler;
     }
-    public void Initialize(IWeapon handler)
+
+
+    public void Initialize(IWeapon weapon)
     {
-        this.weaponHanlder = handler;
+        this.weaponHandler = weapon;
     }
+
+
+
+    private void Awake()
+    {
+        bufferer = GetComponent<InputBufferer>();
+        animator = GetComponent<AnimatorController>();
+
+        execution = GetComponent<AttackExecution>();
+
+        if (execution == null)
+            execution = gameObject.AddComponent<AttackExecution>();
+
+    }
+
 
 
     private void Start()
     {
-        bufferer = GetComponent<InputBufferer>();
-        animator = GetComponent<AnimatorController>();
-        //currentWeapon = FindFirstObjectByType<WeaponHandling>();
+        execution.Initialize(
+            weaponHandler,
+            animator
+        );
     }
+
+
+
 
     private void OnEnable()
     {
-        Ticker.OnTick += OnTick;
+        Ticker.OnTick += Tick;
     }
+
 
     private void OnDisable()
     {
-        Ticker.OnTick -= OnTick;
+        Ticker.OnTick -= Tick;
     }
 
-    private void OnTick()
+
+
+
+    private void Tick()
     {
-        finishedImpacts.Clear();
-
-        foreach (var impact in impacts)
-        {
-            UpdateImpact(impact.Key, impact.Value);
-
-            if (impact.Value.timer >= impact.Value.duration)
-            {
-                finishedImpacts.Add(impact.Key);
-            }
-        }
+        isAttacking = execution.IsExecuting;
 
 
-        foreach (Collider target in finishedImpacts)
-        {
-            impacts.Remove(target);
-        }
-
-
-        if (!isAttacking && !isTransitioning)
+        if (!isAttacking)
         {
             TryStartAttack();
             return;
         }
 
-        UpdateAttack();
+
+        HandleTransitions();
     }
+
+
+
+
 
     private void TryStartAttack()
     {
+        if (!bufferer.HasInput)
+            return;
 
-        if (!bufferer.HasInput) return;
+
+        AttackInput input =
+            bufferer.ConsumeInput();
 
 
-        var input = bufferer.ConsumeInput();
-        Attack attack = GetAttackFromInput(input);
+        Attack attack =
+            GetAttackFromInput(input);
+
+
 
         if (attack == null)
             return;
 
+
+
         StartAttack(attack);
     }
+
+
+
+
+
 
     private Attack GetAttackFromInput(AttackInput input)
     {
@@ -140,33 +137,46 @@ public class CombatStateMachine : MonoBehaviour // ----> “Given state + input 
                 continue;
 
 
+
             if (!HasContext(attack))
                 continue;
 
 
+
             if (attack.DirectionRequired != Vector3.zero)
             {
-                if (Vector3.Dot(
-                    handler.AttackDirection.normalized,
-                    attack.DirectionRequired.normalized
-                    ) < 0.9f)
+                float dot =
+                    Vector3.Dot(
+                        handler.AttackDirection.normalized,
+                        attack.DirectionRequired.normalized
+                    );
+
+
+                if (dot < 0.9f)
                     continue;
             }
+
 
 
             return attack;
         }
 
+
         return null;
     }
 
+
+
+
+
     private bool HasContext(Attack attack)
     {
-        foreach (Attack.Context required in attack.contextRequired)
+        foreach (var required in attack.contextRequired)
         {
             bool found = false;
 
-            foreach (Attack.Context current in handler.currentHandlerContext)
+
+            foreach (var current in handler.currentHandlerContext)
             {
                 if (current == required)
                 {
@@ -176,294 +186,160 @@ public class CombatStateMachine : MonoBehaviour // ----> “Given state + input 
             }
 
 
+
             if (!found)
                 return false;
         }
 
+
         return true;
     }
+
 
     private void StartAttack(Attack attack)
     {
         CurrentAttack = attack;
-        isAttacking = true;
-        isTransitioning = false ;
+
+        isTransitioning = false;
         hasChainedThisWindow = false;
-        //currentWeapon.GetComponent<Collider>().enabled = false ;
-        weaponHanlder.DisableHitbox();
-        weaponHanlder.ClearTargets();
-        hitTargets.Clear();
-        //currentWeapon.Targets.Clear();
-
-        attackStartTick = Ticker.instance.CurrentTick;
-        //CacheAttackData(attack);
-
-        int hash = Animator.StringToHash(attack.clip.name);
-        animator.PlayAttack(hash , attack.AttackIndexLayer , isTransitioning);
-    }
-
-    private void UpdateAttack()
-    {
-        int tick = Ticker.instance.CurrentTick - attackStartTick;
-        CurrentAttackTick = tick;
-        if (isTransitioning)
-            isTransitioning = false;
 
 
-        int totalFrames =
-            CurrentAttack.StartUpFrames +
-            CurrentAttack.ActiveFrames +
-            CurrentAttack.RecoveryFrames;
-
-        //Debug.Log(totalFrames);
-
-        UpdateWeaponState(tick);
-        HandleAttackProperties(CurrentAttack);
-
-        if (DeduceCurrentWindow(tick, CurrentAttack) != WindowType.Interrupt)
-        {
-            hasChainedThisWindow = false;
-        }
-        else
-        {
-            Attack newAttack = GetNewAttack(CurrentAttack);
-
-            if (newAttack != null)
-            {
-                hasChainedThisWindow = true;
-                TransitionAttack(newAttack);
-                return;
-            }
-
-            if (isTryingToCancel)
-                CancelAttack(CurrentAttack);
-        }
-
-
-        if (DeduceCurrentWindow(tick, CurrentAttack) == WindowType.Invulnerability)
-        {
-            Invulnerabilize();
-        }
-
-        if (tick >= totalFrames)
-        {
-            EndAttack();
-            //Debug.Log(tick);
-        }
-    }
-    public WindowType DeduceCurrentWindow(int tick, Attack currentAttack)
-    {
-        if (currentAttack == null)
-            return WindowType.None;
-
-        
-        for (int i = 0; i < currentAttack.FrameWindows.Length; i++)
-        {
-        
-            if (currentAttack.FrameWindows[i].windowType == WindowType.Interrupt &&
-                (tick >= currentAttack.FrameWindows[i].startFrame &&
-                tick <= currentAttack.FrameWindows[i].endFrame))
-                return WindowType.Interrupt;
-
-
-            else if (currentAttack.FrameWindows[i].windowType == WindowType.Invulnerability &&
-                (tick >= currentAttack.FrameWindows[i].startFrame &&
-                tick <= currentAttack.FrameWindows[i].endFrame))
-
-                return WindowType.Invulnerability;
-        }
-
-
-        return WindowType.None;
-    }
-
-    public Attack GetNewAttack(Attack currentAttack)
-    {
-        if (hasChainedThisWindow) return null;
-        if (currentAttack == null) return null;
-        if (currentAttack.AllowedAttackTransitions.Length == 0) return null;
-
-        AttackInput input = bufferer.PeekInput();
-        int i = 0;
-        foreach (Attack transition in currentAttack.AllowedAttackTransitions)
-        {
-            if (
-                transition.RequiredInput == input &&
-                HasContext(transition) &&
-                Vector3.Dot(
-                    handler.AttackDirection.normalized,
-                    transition.DirectionRequired.normalized
-                ) > 0.9f
-            )
-            {
-                bufferer.ConsumeInput();
-                return transition;
-            }
-            i++;
-        }
-
-        return null;
-    }
-    public void TransitionAttack(Attack newAttack)
-    {
-        CurrentAttack = newAttack;
-        attackStartTick = Ticker.instance.CurrentTick;
-        isAttacking = true;
-        isTransitioning = true;
-        hasChainedThisWindow = false;
-        //Debug.Log($"Chaining into  {CurrentAttack.name}");
-        int hash = Animator.StringToHash(newAttack.clip.name);
-        animator.PlayAttack(hash , newAttack.AttackIndexLayer , isTransitioning);
+        execution.StartAttack(
+            attack
+        );
     }
 
 
-    public void CancelAttack(Attack currentAttack)
+
+    private void HandleTransitions()
     {
-        //Interrupt Attack with a different move (i.e. Jump  , parry etc) 
-        return;
-    }
-
-
-    private void Invulnerabilize()
-    {
-        // Invincible during these Frames
-    }
-
-
-    private void UpdateWeaponState(int tick)
-    {
-        int activeStart = CurrentAttack.StartUpFrames;
-        int activeEnd = activeStart + CurrentAttack.ActiveFrames;
-
-
-        if (tick >= activeStart && tick <= activeEnd)
-        {
-            weaponHanlder.EnableHitbox();
-        }
-        else
-        {
-            weaponHanlder.DisableHitbox();
-        }
-    }
-
-
-    private void HandleAttackProperties(Attack currentAttack)
-    {
-        foreach (Collider target in targetsHit)
-        {
-            if (hitTargets.Contains(target))
-                continue;
-
-            hitTargets.Add(target);
-
-            if (shouldLaunchTarget)
-                StartLaunch(target, currentAttack);
-
-            if (!shouldLaunchTarget)
-                ApplyKnockBack(target, currentAttack);
-
-            Health health = target.GetComponent<Health>();
-
-            if (health != null)
-                health.TakeDamage(currentAttack.Damage);
-
-            StartCoroutine(ApplyHitStop(currentAttack));
-        }
-    }
-
-    private void StartLaunch(Collider target, Attack attack)
-    {
-        Vector3 start = target.transform.position;
-
-        Vector3 end = start + Vector3.up * attack.upwardLaunchImpulse;
-
-        impacts[target] = new ImpactData
-        {
-            start = start,
-            end = end,
-            timer = 0,
-            duration = attack.attackSpeed,
-            curve = attack.lungeCurve
-        };
-    }
-    private void ApplyKnockBack(Collider target, Attack attack)
-    {
-        Vector3 start = target.transform.position;
-
-        Vector3 direction =
-            (target.transform.position - transform.position).normalized;
-
-
-        Vector3 end =
-            start + direction * attack.knockBackInflicted;
-
-
-        impacts[target] = new ImpactData
-        {
-            start = start,
-            end = end,
-            timer = 0,
-            duration = attack.attackSpeed,
-            curve = attack.lungeCurve
-        };
-    }
-
-    private void ApplyHitStun(Collider target , Attack attack)
-    {
-
-    }
-
-
-    private void UpdateImpact(Collider target, ImpactData data)
-    {
-        if (target == null)
+        if (CurrentAttack == null)
             return;
 
 
-        data.timer += Time.deltaTime;
+
+        if (
+            execution.DeduceCurrentWindow(
+                execution.CurrentTick,
+                CurrentAttack
+            )
+            != WindowType.Interrupt
+        )
+        {
+            hasChainedThisWindow = false;
+            return;
+        }
 
 
-        float t =
-            Mathf.Clamp01(data.timer / data.duration);
 
 
-        float eased =
-            data.curve.Evaluate(t);
+
+        Attack next =
+            GetNewAttack(CurrentAttack);
 
 
-        target.transform.position =
-            Vector3.Lerp(
-                data.start,
-                data.end,
-                eased
-            );
 
+        if (next != null)
+        {
+            hasChainedThisWindow = true;
+
+            TransitionAttack(next);
+        }
     }
 
-    private IEnumerator ApplyHitStop(Attack attack)
+
+
+    public Attack GetNewAttack(Attack currentAttack)
     {
-        Time.timeScale = 0f;
-        yield return new WaitForSecondsRealtime(attack.hitStopDuration);
-        Time.timeScale = 1f;
+        if (hasChainedThisWindow)
+            return null;
+
+
+        if (currentAttack == null)
+            return null;
+
+
+
+        if (currentAttack.AllowedAttackTransitions.Length == 0)
+            return null;
+
+
+
+
+        AttackInput input =
+            bufferer.PeekInput();
+
+
+
+
+        foreach (Attack transition in currentAttack.AllowedAttackTransitions)
+        {
+
+            if (
+                transition.RequiredInput == input &&
+                HasContext(transition)
+            )
+            {
+
+                if (transition.DirectionRequired != Vector3.zero)
+                {
+
+                    float dot =
+                        Vector3.Dot(
+                        handler.AttackDirection.normalized,
+                        transition.DirectionRequired.normalized
+                        );
+
+
+                    if (dot < 0.9f)
+                        continue;
+                }
+
+
+
+                bufferer.ConsumeInput();
+
+                return transition;
+            }
+
+        }
+
+
+
+        return null;
     }
 
-    private void EndAttack()
+
+
+
+
+
+    public void TransitionAttack(Attack next)
     {
-        int hash = Animator.StringToHash(CurrentAttack.clip.name);
-        animator.StopAttack(hash);
+        if (next == null)
+            return;
 
-        CurrentAttack = null;
-        isAttacking = false;
-        isTransitioning = false;
-        hasChainedThisWindow = false;
-        attackStartTick = 0;
-        isTargetHit = false;
-        weaponHanlder.DisableHitbox();
-        weaponHanlder.ClearTargets();
-        hitTargets.Clear();
-        //currentWeapon.GetComponent<Collider>().enabled = false;
-        //currentWeapon.Targets.Clear();
+
+
+        CurrentAttack = next;
+
+        isTransitioning = true;
+
+
+        execution.StartAttack(
+            next,
+            true
+        );
     }
 
+
+
+
+    public void CancelAttack()
+    {
+        // Future:
+        // dodge
+        // parry
+        // jump cancel
+    }
 }
-
